7f5b60e6807f02bad2fe67a86103907204df5f82
Arie May Wibowo <ariemayw@gmail.com>
2020-10-27 15:51:32 +0700

    added firestore

---
+++ triafriends/Helper/FirestoreController.swift
@@ -0,0 +1,33 @@
+//
+//  FirestoreController.swift
+//  triafriends
+//
+//  Created by Arie May Wibowo on 27/10/20.
+//
+
+import SwiftUI
+import Firebase
+
+class FirestoreController {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    func storeDoctor(name: String, hospital: String) {
+        
+        let db = Firestore.firestore()
+        // Add a new document with a generated ID
+        var ref: DocumentReference? = nil
+        ref = db.collection("doctors").addDocument(data: [
+            "name": name,
+            "hospital": hospital
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+    }
+}
